714571fbba7ee8da3a8c56d26ca56f374bfa9cdb
alexandre zenas <alexandrezenas11@gmail.com>
2025-02-18 19:01:16 -0300

    Feat: headline hero

--- src/pages/home/hero/index.jsx
+++ src/pages/home/hero/index.jsx
@@ -6,7 +6,7 @@ export function Hero(){
             <div className="max-w-[1280px] mx-auto px-[2%] flex flex-col-reverse lg:flex-row lg:justify-between justify-center items-center py-24 gap-8">
                 <div className="lg:w-3/5 flex flex-col lg:justify-start justify-center lg:items-start items-center gap-8">
                     <h1 className="lg:text-start text-center font-medium xl:text-5xl md:text-4xl text-3xl">Quer ter um site  personalizado  desenvolvido por um <strong className="font-bold">programador profissional</strong> ?</h1>
-                    <p className="lg:text-start text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed tellus ac urna placerat porttitor. Duis vestibulum ultrices quam et ultrices. Nulla lobortis nunc in metus. </p>
+                    <p className="lg:text-start text-center text-[#828282]">Tenha um site adaptado às necessidades da sua empresa – feito sob medida por um desenvolvedor web e destaque o seu negócio na internet!</p>
                     <MainButton 
                         textButton={'Fazer um orçamento'}
                         linkButton={'#Contact'}
