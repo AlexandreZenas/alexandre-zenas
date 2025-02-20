6be71e6b9ae809b87051b4106419596f90b4f778
alexandre zenas <alexandrezenas11@gmail.com>
2025-02-20 12:37:57 -0300

    feat: copy cards altered

--- src/pages/home/benefits/index.jsx
+++ src/pages/home/benefits/index.jsx
@@ -9,21 +9,21 @@ export function Benefits(){
             <div className={`max-w-[1280px] mx-auto px-[2%]  flex-col lg:flex-row lg:flex-nowrap flex-wrap flex   gap-8 `}>
                 <Card 
                     animateCard={'animate-elementIn'}
-                    imgPath={"icons/cards/web.svg"}
-                    title={'Seu negócio online'}
-                    text={'Ter um site permite que você tenha presença na internet. Isso é crucial para os dias de hoje, já que muitas pessoas procuram por produtos e serviços online antes de  fazer uma compra.'}
+                    imgPath={"icons/cards/customization.svg"}
+                    title={'Personalização Total'}
+                    text={'Um programador pode criar um site 100% personalizado, com design exclusivo, sem limitações de templates genéricos ou funcionalidades pré-definidas.'}
                 />
                 <Card 
                     animateCard={'lg:animate-elementInLow  animate-elementIn'}
-                    imgPath={"icons/cards/search.svg"}
-                    title={'No topo das pesquisas'}
-                    text={'Utilizando técnicas de SEO e anúncios para aumentar a visibilidade nos mecanismos de busca, o fluxo de usuários e a taxa de conversão do site aumente exponencialmente.'}
+                    imgPath={"icons/cards/performance.svg"}
+                    title={'Melhor Performance'}
+                    text={'Sites desenvolvidos sob medida são mais rápidos, leves e otimizados, já que não carregam códigos  e funcionalidades desnecessárias.'}
                 />
                     <Card 
                     animateCard={'lg:animate-elementInVeryLow animate-elementIn'}
-                    imgPath={"icons/cards/laptop.svg"}
-                    title={'Um programador ao seu dispor'}
-                    text={'Em nossa consultoria juntos Desenvolvemos  estratégias eficazes e personalizadas identificando oportunidades até a execução de campanhas eficazes.'}
+                    imgPath={"icons/cards/security.svg"}
+                    title={'Maior Segurança'}
+                    text={'Sites personalizados são menos vulneráveis a ataques, pois não dependem de plugins de terceiros frequentemente explorados por hackers.'}
                 />
             </div>
         </section>
