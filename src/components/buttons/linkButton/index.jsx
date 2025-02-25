91a33db81ed0b4d9f2fe4e0d71dbddc365ca63b2
alexandre zenas <alexandrezenas11@gmail.com>
2025-02-25 16:15:51 -0300

    feat: add tag conversion in buttons

--- src/components/buttons/linkButton/index.jsx
+++ src/components/buttons/linkButton/index.jsx
@@ -1,9 +1,18 @@
 import { twMerge } from "tailwind-merge";
 
 export function LinkButton({link, textLink, target, buttonStyle}){
+    function clickAds(){
+        return gtag_report_conversion(link);
+      }
     return(
-    <div className={twMerge(`wrapper-line `, buttonStyle)}>
-        <a className="inline-block text-base font-medium leading-loose text-blue" href={link} target={target} >{textLink}</a>
+    <div className={twMerge(`wrapper-line hover:cursor-pointer`, buttonStyle)}>
+        <a 
+            className="inline-block text-base font-medium leading-loose text-blue" 
+            target={target} 
+            onClick={clickAds}
+        >
+            {textLink}
+        </a>
         <img className="inline-block  pl-4" src="icons/cards/arrow.svg" alt="" />
         <div className="w-full h-[2px] bg-[#8ab7f1] rounded-3xl">
             <div className="line w-[0%] h-full bg-blue dark:bg-white rounded-3xl"></div>
--- src/components/buttons/main-button/index.jsx
+++ src/components/buttons/main-button/index.jsx
@@ -1,8 +1,16 @@
 import { twMerge } from "tailwind-merge";
 
 export function MainButton({textButton, imgPath,imgAlt, styleButton, styleBTN, target, linkButton}){
+
+    function clickAds(){
+        return gtag_report_conversion(linkButton);
+      }
     return(
-            <a className={twMerge("py-4 px-10 hover:text-gray transition-all text-white font-semibold bg-blue rounded-xl", styleButton)} target={target} href={linkButton}>
+            <a 
+                className={twMerge("hover:cursor-pointer py-4 px-10 hover:text-gray transition-all text-white font-semibold bg-blue rounded-xl", styleButton)}
+                target={target} 
+                onClick={clickAds}
+            >
                 <button className={styleBTN}>
                     <img src={imgPath} alt={imgAlt} />
                     {textButton}
